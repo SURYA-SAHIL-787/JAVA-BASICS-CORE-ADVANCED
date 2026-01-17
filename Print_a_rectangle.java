393b3f1c6ebdb0cc598094d0c4c7f46f459f9d21
SURYA SAHIL - 787 <suryasahil878@gmail.com>
2026-01-17 18:00:43 +0530

    Add files via upload

---
+++ Print_a_rectangle.java
@@ -0,0 +1,11 @@
+public class Print_a_rectangle
+ {
+    public static void main (String[]args)
+    {
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        
+    }
+    
+}
