63888d3a4c6db1efb89aab6aa46b976be6876de9
SURYA SAHIL - 787 <suryasahil878@gmail.com>
2026-03-17 16:13:25 +0530

    Create DetectCycleLinkedList.java

---
+++ DetectCycleLinkedList.java
@@ -0,0 +1,40 @@
+public class DetectCycleLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static boolean hasCycle(Node head) {
+        if (head == null) return false;
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;         
+            fast = fast.next.next;    
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        head.next.next.next.next = head.next; // cycle created
+
+        System.out.println("Has Cycle: " + hasCycle(head));
+    }
+}
