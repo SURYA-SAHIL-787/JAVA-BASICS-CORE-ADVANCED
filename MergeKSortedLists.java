7a12928fed2987de1a3b87dfc4ae6f8594a7b00b
SURYA SAHIL - 787 <suryasahil878@gmail.com>
2026-03-26 15:48:59 +0530

    Create MergeKSortedLists.java

---
+++ MergeKSortedLists.java
@@ -0,0 +1,64 @@
+import java.util.PriorityQueue;
+
+public class MergeKSortedLists {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode smallest = pq.poll();
+            current.next = smallest;
+            current = current.next;
+
+            if (smallest.next != null) {
+                pq.offer(smallest.next);
+            }
+        }
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode l3 = new ListNode(2);
+        l3.next = new ListNode(6);
+
+        ListNode[] lists = {l1, l2, l3};
+
+        ListNode result = mergeKLists(lists);
+        printList(result); // 1 1 2 3 4 4 5 6
+    }
+}
