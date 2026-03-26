2eefe5446dce6be42eca000b8a31d403c2d698b4
SURYA SAHIL - 787 <suryasahil878@gmail.com>
2026-03-26 15:48:12 +0530

    Create LRUCache.java

---
+++ LRUCache.java
@@ -0,0 +1,96 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCache {
+
+    class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int capacity;
+    private final Map<Integer, Node> map;
+    private final Node head;
+    private final Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new HashMap<>();
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void addNode(Node node) {
+        Node nextNode = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = nextNode;
+        nextNode.prev = node;
+    }
+
+    private void removeNode(Node node) {
+        Node prevNode = node.prev;
+        Node nextNode = node.next;
+        prevNode.next = nextNode;
+        nextNode.prev = prevNode;
+    }
+
+    private void moveToFront(Node node) {
+        removeNode(node);
+        addNode(node);
+    }
+
+    private Node removeLRU() {
+        Node lru = tail.prev;
+        removeNode(lru);
+        return lru;
+    }
+
+    public int get(int key) {
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+        Node node = map.get(key);
+        moveToFront(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            moveToFront(node);
+        } else {
+            if (map.size() == capacity) {
+                Node lru = removeLRU();
+                map.remove(lru.key);
+            }
+            Node newNode = new Node(key, value);
+            addNode(newNode);
+            map.put(key, newNode);
+        }
+    }
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+
+        cache.put(1, 10);
+        cache.put(2, 20);
+        System.out.println(cache.get(1)); // 10
+
+        cache.put(3, 30); // removes key 2
+        System.out.println(cache.get(2)); // -1
+
+        cache.put(4, 40); // removes key 1
+        System.out.println(cache.get(1)); // -1
+        System.out.println(cache.get(3)); // 30
+        System.out.println(cache.get(4)); // 40
+    }
+}
