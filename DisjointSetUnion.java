a2278696478d8dba25d061ab07c2c450a73d1dff
SURYA SAHIL - 787 <suryasahil878@gmail.com>
2026-04-17 10:41:18 +0530

    Implement Disjoint Set Union with union by rank and size

---
+++ DisjointSetUnion.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+public class DisjointSetUnion {
+    static class DisjointSet {
+        int[] parent, rank, size;
+
+        DisjointSet(int n) {
+            parent = new int[n + 1];
+            rank = new int[n + 1];
+            size = new int[n + 1];
+
+            for (int i = 0; i <= n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        int findUPar(int node) {
+            if (node == parent[node]) return node;
+            return parent[node] = findUPar(parent[node]);
+        }
+
+        void unionByRank(int u, int v) {
+            int pu = findUPar(u);
+            int pv = findUPar(v);
+
+            if (pu == pv) return;
+
+            if (rank[pu] < rank[pv]) {
+                parent[pu] = pv;
+            } else if (rank[pv] < rank[pu]) {
+                parent[pv] = pu;
+            } else {
+                parent[pv] = pu;
+                rank[pu]++;
+            }
+        }
+
+        void unionBySize(int u, int v) {
+            int pu = findUPar(u);
+            int pv = findUPar(v);
+
+            if (pu == pv) return;
+
+            if (size[pu] < size[pv]) {
+                parent[pu] = pv;
+                size[pv] += size[pu];
+            } else {
+                parent[pv] = pu;
+                size[pu] += size[pv];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DisjointSet ds = new DisjointSet(7);
+        ds.unionBySize(1, 2);
+        ds.unionBySize(2, 3);
+        ds.unionBySize(4, 5);
+        ds.unionBySize(6, 7);
+        ds.unionBySize(5, 6);
+
+        System.out.println(ds.findUPar(3) == ds.findUPar(7) ? "Same" : "Not Same");
+
+        ds.unionBySize(3, 7);
+
+        System.out.println(ds.findUPar(3) == ds.findUPar(7) ? "Same" : "Not Same");
+    }
+}
