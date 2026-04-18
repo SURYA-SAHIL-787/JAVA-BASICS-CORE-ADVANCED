e87842012078ce1ab425aab1cd7775db31668e30
SURYA SAHIL - 787 <suryasahil878@gmail.com>
2026-04-18 22:33:33 +0530

    Add QuickSortDescending class with sorting logic
    Implement QuickSort algorithm to sort an array in descending order.

---
+++ QuickSortDescending.java
@@ -0,0 +1,48 @@
+public class QuickSortDescending {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] > pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr)
+            System.out.print(num + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 4, 7, 19, 3, 15};
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted Array in Descending Order:");
+        printArray(arr);
+    }
+}
